8fa8445807b036b975f9fac42c511385164e7766
eiranstudio <iradukundaeiran@gmail.com>
2023-11-05 02:23:05 -0800

    make express server

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
-import job from "./keepAlive.js";
 const express = require('express')
 const app = express()
 const port = 3000
 
+const cron = require("cron")
+const axios = require("axios").default
+
+const backendUrl = process.env.BACKEND_URL;
+const agent = axios.create({
+    baseURL: backendUrl,
+})
+
+const job = new cron.CronJob('*/1 * * * *', function () {
+    // This function will be executed every 14 minutes.
+    console.log("Restarting server");
+    // Perform an HTTPS GET request to hit any backend api.
+    agent.get('/').then((Response)=>{
+        if(Response.status == 200){
+            console.log(`Server restarted: ${Response.status}`);
+        }else{
+            console.log(`Failed to restart server with status code: ${Response.status}`)
+        }
+    }).catch((err)=>{
+        console.log('Error during Restart: ', err.message)
+    })
+});
+
 job.start()
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello Eiran!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Hello Eiran, iam listening on port: ${port}`)
 })
